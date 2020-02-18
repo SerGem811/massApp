3d1c0b132b8e5e9dee9a84b5963efcf366b9dde8
serGem811 <ser.gem.811@outlook.com>
2020-02-18 15:35:31 +0800

    update for aws

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -3,7 +3,7 @@ const BASE_URL = 'http://13.59.11.156:1337';
 
 const WAPythonURL = 'http://13.59.11.156:8001';
 
-const WAGOURL = 'http://13.59.11.156:4000';
+const WAGOURL = 'http://172.107.180.59:4000';
 
 const TGPythonURL = 'http://13.59.11.156:8000';
 
